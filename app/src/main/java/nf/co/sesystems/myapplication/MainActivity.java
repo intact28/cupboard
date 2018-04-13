2e7ecbe8cf57c3f65253192cc39688dacedc4dbe
Oskar Test <oskartest@mail.com>
2018-04-13 14:45:24 +0200

    Added confimation dialog to delete action

--- app/src/main/java/nf/co/sesystems/myapplication/MainActivity.java
+++ app/src/main/java/nf/co/sesystems/myapplication/MainActivity.java
@@ -1,8 +1,10 @@
 package nf.co.sesystems.myapplication;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
@@ -84,8 +86,23 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void removeItem(int position) {
-        itemList.remove(position);
-        mAdapter.notifyDataSetChanged();
+    public void removeItem(final int position) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Delete item?")
+                .setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        itemList.remove(position);
+                        mAdapter.notifyDataSetChanged();
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+        builder.create().show();
+
     }
 }
